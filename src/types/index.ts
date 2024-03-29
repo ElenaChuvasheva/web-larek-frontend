3ab059818ebc8784878a4e859ce1aa4e7ac61300
ElenaChuvasheva <chuvashevalena@gmail.com>
2024-03-29 09:21:08 +0300

    docs

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,5 @@
-export interface IItem {
+// модель товара
+interface IItemModel {
     id: string;
     description?: string;
     image: string;
@@ -7,11 +8,8 @@ export interface IItem {
     price: number;
 }
 
-type PaymentType = 'online' | 'ondelivery';
-
-type Uuid = string;
-
-export interface IOrder {
+// данные заказа для отправки на бэкенд
+type IOrder = {
     payment: PaymentType;
     email: string;
     phone: string;
@@ -20,10 +18,45 @@ export interface IOrder {
     items: Uuid[];
 }
 
-export interface IAppState {
-    catalog: IItem[];
+// модель состояния приложения
+interface IAppStateModel {
+    catalog: IItemModel[];
     shoppingCart: Uuid[];
     order: IOrder | null;
 }
 
-export type IBasketItem = Pick<IItem, 'title' | 'price'> & { isInShoppingCart: boolean; }
+type PaymentType = 'online' | 'ondelivery';
+
+type Uuid = string;
+
+// данные для отображения товара в корзине
+type IShoppingCartItem = Pick<IItemModel, 'title' | 'price'>
+
+// главная страница
+interface IMainPage {
+    cartCounter: number;
+    catalog: HTMLElement[];    
+}
+
+// для отображения корзины
+interface IShoppingCart {
+    items: HTMLElement[];
+    total: number;
+}
+
+// для формы заказа
+interface IOrderForm {
+    address: string;
+}
+
+// для формы контактов
+interface IContactForm {
+    email: string;
+    phone: string;
+}
+
+// состояние формы
+interface IFormState {
+    valid: boolean | null;
+    errors: string[];
+}
